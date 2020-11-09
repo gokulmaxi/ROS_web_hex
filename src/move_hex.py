c60a1c590c58af62991baf5979e3036edfd2b63f
gokulROS <gokulros@pop-os.localdomain>
2020-11-09 22:14:55 +0530

    perfected the shape

--- src/move_hex.py
+++ src/move_hex.py
@@ -1,60 +1,48 @@
 #!/usr/bin/env python3
 import rospy
 from geometry_msgs.msg import Twist
+from turtlesim.msg import Pose
 from std_msgs.msg import String
 import time
+import math
+
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-    move()
+	x=turtlesim()
+	x.move()
 def listener():
 
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
     rospy.init_node('listener', anonymous=True)
 
     rospy.Subscriber("chatter", String, callback)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
-def move():
-    # Starts a new node
-    velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
-    vel_msg = Twist()
-
-    #Receiveing the user's input
-    print("Let's move your robot")
-
-    #Checking if the movement is forward or backwards
-
-    vel_msg.linear.x = 5
-    #Since we are moving just in x-axis
-    vel_msg.linear.y = 0
-    vel_msg.linear.z = 0
-    vel_msg.angular.x = 0
-    vel_msg.angular.y = 0
-    vel_msg.angular.z = 0
-
-    
-    #Setting the current time for distance calculus
-    for i in range(6):
-        velocity_publisher.publish(vel_msg)
-        time.sleep(0.5)
-        vel_msg.linear.x = 0
-        #Force the robot to stop
-        velocity_publisher.publish(vel_msg)
-        time.sleep(0.5)
-        vel_msg.angular.z=10
-        velocity_publisher.publish(vel_msg)
-        time.sleep(0.5)
-        vel_msg.angular.z=0
-        vel_msg.linear.x=5
-        print(i)
-            
-
-
+class turtlesim:
+	def __init__(self):
+		self.velocity_publisher=rospy.Publisher('/turtle1/cmd_vel',Twist, queue_size=10)
+		self.pose_subscriber = rospy.Subscriber('/turtle1/pose', Pose, self.poseCallback)
+		self.pose = Pose()
+	def poseCallback(self,data):
+		self.pose.theta=round(data.theta,2)
+	def move(self):
+		vel_msg=Twist()
+		rate =rospy.Rate(1)
+		for i in range(0,6,1):
+			vel_msg.linear.x =0
+			vel_msg.angular.z=1.0472/1
+			self.velocity_publisher.publish(vel_msg)
+			rate.sleep()
+			vel_msg.linear.x=1
+			vel_msg.angular.z=0
+			self.velocity_publisher.publish(vel_msg)
+			rate.sleep()
+		vel_msg.linear.x=0
+		vel_msg.linear.y=0
+		vel_msg.angular.y=0
+		vel_msg.angular.z=0
+		self.velocity_publisher.publish(vel_msg)
+		rospy.loginfo("completed")
+		rate.sleep()
 if __name__ == '__main__':
     try:
         #Testing our function
